73e106c8600c5bc87331ba83ec05766c57d8786a
Sergio Borbone <sergioborbone@gmail.com>
2022-03-05 19:15:15 +0100

    Create card articoli blog nel main

---
+++ src/components/cardArticle.vue
@@ -0,0 +1,100 @@
+<template>
+  <section id="cardComponent">
+
+      <!-- ARTICLES -->
+      <div class="card-article">
+
+          <div class="text-wrap">
+              <span class="tag-article">Photography</span>
+              <h3><a href="">How To Take Better Concert Pictures in 30 Seconds</a></h3>
+              <div class="sub-text">
+                <h4>Euismod atras vulputate iltricies etri elit. Class aptent taciti sociosqu 
+                ad litora torquent per conubia nostra, per inceptos himenaeos.</h4>
+              </div>
+          </div>
+          
+          <img src="../assets/img/blog-46.jpg" alt="article-image">
+      </div>
+
+      <div class="card-article">
+
+          <div class="text-wrap">
+              <span class="tag-article">Gadgets</span>
+              <h3><a href="">Gadgets That Make Your Smartphone Even Smarter</a></h3>
+              <div class="sub-text">
+                <h4>Euismod atras vulputate iltricies etri elit. Class aptent taciti sociosqu 
+                ad litora torquent per conubia nostra, per inceptos himenaeos.</h4>
+              </div>
+          </div>
+
+          <img src="../assets/img/blog-47.jpg" alt="article-image">
+      </div>
+
+      <div class="card-article">
+
+          <div class="text-wrap">
+              <span class="tag-article">Travel</span>
+              <h3><a href="">20 Top-Rated Tourist Attractions in Manhattan</a></h3>
+              <div class="sub-text">
+                <h4>Euismod atras vulputate iltricies etri elit. Class aptent taciti sociosqu 
+                ad litora torquent per conubia nostra, per inceptos himenaeos.</h4>
+              </div>
+          </div>
+
+          <img src="../assets/img/blog-48.jpg" alt="article-image">
+      </div>
+
+      <div class="card-article">
+
+          <div class="text-wrap">
+              <span class="tag-article">Lifestyle</span>
+              <h3><a href="">The Best Way to Ride a Motorcycle</a></h3>
+              <div class="sub-text">
+                <h4>Euismod atras vulputate iltricies etri elit. Class aptent taciti sociosqu 
+                ad litora torquent per conubia nostra, per inceptos himenaeos.</h4>
+              </div>
+          </div>
+
+          <img src="../assets/img/blog-49.jpg" alt="article-image">
+      </div>
+
+      <div class="card-article">
+
+          <div class="text-wrap">
+              <span class="tag-article">Travel</span>
+              <h3><a href="">5 Fun Things to Do at the Beach</a></h3>
+              <div class="sub-text">
+                <h4>Euismod atras vulputate iltricies etri elit. Class aptent taciti sociosqu 
+                ad litora torquent per conubia nostra, per inceptos himenaeos.</h4>
+              </div>
+          </div>
+
+          <img src="../assets/img/blog-50.jpg" alt="article-image">
+      </div>
+
+      <div class="card-article">
+
+          <div class="text-wrap">
+              <span class="tag-article">Recipes</span>
+              <h3><a href="">Amazingly Fresh Fruit And Herb Drinks For Summer</a></h3>
+              <div class="sub-text">
+                <h4>Euismod atras vulputate iltricies etri elit. Class aptent taciti sociosqu 
+                ad litora torquent per conubia nostra, per inceptos himenaeos.</h4>
+              </div>
+          </div>
+
+          <img src="../assets/img/blog-51.jpg" alt="article-image">
+      </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'cardArticle',
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../assets/style/cardArticle.scss';
+</style>
--- src/components/mainPorto.vue
+++ src/components/mainPorto.vue
@@ -1,15 +1,32 @@
 <template>
   <main>
+
+    <section>
+      <div id="jumbotron">
+
+        <div class="container-w75">
+            <cardArticle></cardArticle>
+        </div>
+
+      </div>
+    </section>
+
   </main>
 </template>
 
 <script>
+
+import cardArticle from './cardArticle.vue'
+
 export default {
   name: 'mainPorto',
+  components: {
+    cardArticle,
+  },
 
 }
 </script>
 
 <style lang="scss" scoped>
-
+  @import '../assets/style/mainPorto.scss';
 </style>
