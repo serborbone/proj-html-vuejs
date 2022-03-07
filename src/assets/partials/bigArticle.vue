9a42315131518ecf2b7fb9ee37f9da51792b533b
Sergio Borbone <sergioborbone@gmail.com>
2022-03-07 14:21:15 +0100

    Creata col con articoli grandi

--- src/assets/partials/bigArticle.vue
+++ src/assets/partials/bigArticle.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="bigArticle">
 
+    <!-- CONTAINER ARTICLE -->
     <div class="article-wrap">
 
         <div class="img-bigart">
@@ -57,6 +58,248 @@
         </div>
 
     </div>
+    <!-- FINE CONTAINER ARTICLE -->
+
+    <!-- CONTAINER ARTICLE -->
+    <div class="article-wrap">
+
+        <div class="img-bigart">
+            <img src="../img/blog-55 (1).jpg" alt="article image">
+        </div>
+
+        <div class="info-art">
+
+            <div class="date-art">
+                <div class="day">
+                  12
+                </div>
+                <div class="month">
+                  Jan
+                </div>
+            </div>
+
+            <div class="text-art">
+                <!-- TITLE -->
+                <h3>Simple Ways to Have a Pretty Face</h3>
+
+                <!-- TEXT ARTICLE -->
+                <p>Euismod atras vulputate iltricies etri elit. 
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
+                per inceptos himenaeos. Nulla nunc dui, tristique in semper vel, 
+                congue sed ligula. Nam dolor ligula, faucibus id sodales in, 
+                auctor fringilla libero. Pellentesque pellentesque tempor tellus 
+                eget hendrerit. Morbi id aliquam ligula. Aliquam id dui sem. 
+                Proin rhoncus consequat nisl, eu ornare mauris tincidunt vitae.[...]</p>
+
+                <div class="info-text">
+
+                  <span>
+                    <i class="i-color fa-solid fa-user-large"></i>  
+                    By <a href="#">John Doe</a></span>
+                  <span>
+                    <i class="i-color fa-solid fa-folder"></i> 
+                    <a href="#">Photography</a>, <a href="#">Travel</a>
+                  </span>
+
+                  <span>
+                    <i class="i-color fa-solid fa-comments"></i>
+                    <a href="#">12 Comments</a>
+                  </span>
+
+                  <!-- READ MORE BUTTON -->
+                  <span class="read-btn">
+                      <a href="#">Read More</a>
+                  </span>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <!-- FINE CONTAINER ARTICLE -->
+
+    <!-- CONTAINER ARTICLE -->
+    <div class="article-wrap">
+
+        <div class="img-bigart">
+            <img src="../img/blog-56 (1).jpg" alt="article image">
+        </div>
+
+        <div class="info-art">
+
+            <div class="date-art">
+                <div class="day">
+                  12
+                </div>
+                <div class="month">
+                  Jan
+                </div>
+            </div>
+
+            <div class="text-art">
+                <!-- TITLE -->
+                <h3>Ranking the greatest players in basketball</h3>
+
+                <!-- TEXT ARTICLE -->
+                <p>Euismod atras vulputate iltricies etri elit. 
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
+                per inceptos himenaeos. Nulla nunc dui, tristique in semper vel, 
+                congue sed ligula. Nam dolor ligula, faucibus id sodales in, 
+                auctor fringilla libero. Pellentesque pellentesque tempor tellus 
+                eget hendrerit. Morbi id aliquam ligula. Aliquam id dui sem. 
+                Proin rhoncus consequat nisl, eu ornare mauris tincidunt vitae.[...]</p>
+
+                <div class="info-text">
+
+                  <span>
+                    <i class="i-color fa-solid fa-user-large"></i>  
+                    By <a href="#">John Doe</a></span>
+                  <span>
+                    <i class="i-color fa-solid fa-folder"></i> 
+                    <a href="#">Sport</a>, <a href="#">Travel</a>
+                  </span>
+
+                  <span>
+                    <i class="i-color fa-solid fa-comments"></i>
+                    <a href="#">12 Comments</a>
+                  </span>
+
+                  <!-- READ MORE BUTTON -->
+                  <span class="read-btn">
+                      <a href="">Read More</a>
+                  </span>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <!-- FINE CONTAINER ARTICLE -->
+
+    <!-- CONTAINER ARTICLE -->
+    <div class="article-wrap">
+
+        <div class="img-bigart">
+            <img src="../img/blog-58 (1).jpg" alt="article image">
+        </div>
+
+        <div class="info-art">
+
+            <div class="date-art">
+                <div class="day">
+                  12
+                </div>
+                <div class="month">
+                  Jan
+                </div>
+            </div>
+
+            <div class="text-art">
+                <!-- TITLE -->
+                <h3>Top Camper Trailer Towing Tips</h3>
+
+                <!-- TEXT ARTICLE -->
+                <p>Euismod atras vulputate iltricies etri elit. 
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
+                per inceptos himenaeos. Nulla nunc dui, tristique in semper vel, 
+                congue sed ligula. Nam dolor ligula, faucibus id sodales in, 
+                auctor fringilla libero. Pellentesque pellentesque tempor tellus 
+                eget hendrerit. Morbi id aliquam ligula. Aliquam id dui sem. 
+                Proin rhoncus consequat nisl, eu ornare mauris tincidunt vitae.[...]</p>
+
+                <div class="info-text">
+
+                  <span>
+                    <i class="i-color fa-solid fa-user-large"></i>  
+                    By <a href="#">John Doe</a></span>
+                  <span>
+                    <i class="i-color fa-solid fa-folder"></i> 
+                    <a href="#">Travel</a>, <a href="#">Lifestyle</a>
+                  </span>
+
+                  <span>
+                    <i class="i-color fa-solid fa-comments"></i>
+                    <a href="#">12 Comments</a>
+                  </span>
+
+                  <!-- READ MORE BUTTON -->
+                  <span class="read-btn">
+                      <a href="">Read More</a>
+                  </span>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <!-- FINE CONTAINER ARTICLE -->
+
+    <!-- CONTAINER ARTICLE FOUR IMAGE -->
+    <div class="article-wrap">
+
+        <div class="four-img">
+            <img src="../img/blog-13.jpg" alt="article image">
+            <img src="../img/blog-16.jpg" alt="article image">
+            <img src="../img/blog-20.jpg" alt="article image">
+            <img src="../img/blog-23.jpg" alt="article image">
+            <img src="../img/blog-29.jpg" alt="article image">
+            <img src="../img/blog-40.jpg" alt="article image">
+        </div>
+
+        <div class="info-art">
+
+            <div class="date-art">
+                <div class="day">
+                  8
+                </div>
+                <div class="month">
+                  Jan
+                </div>
+            </div>
+
+            <div class="text-art">
+                <!-- TITLE -->
+                <h3>10 Best Travel Tips After 5 Years Traveling The World</h3>
+
+                <!-- TEXT ARTICLE -->
+                <p>Euismod atras vulputate iltricies etri elit. 
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
+                per inceptos himenaeos. Nulla nunc dui, tristique in semper vel, 
+                congue sed ligula. Nam dolor ligula, faucibus id sodales in, 
+                auctor fringilla libero. Pellentesque pellentesque tempor tellus 
+                eget hendrerit. Morbi id aliquam ligula. Aliquam id dui sem. 
+                Proin rhoncus consequat nisl, eu ornare mauris tincidunt vitae.[...]</p>
+
+                <div class="info-text">
+
+                  <span>
+                    <i class="i-color fa-solid fa-user-large"></i>  
+                    By <a href="#">John Doe</a></span>
+                  <span>
+                    <i class="i-color fa-solid fa-folder"></i> 
+                    <a href="#">Travel</a>, <a href="#">Lifestyle</a>
+                  </span>
+
+                  <span>
+                    <i class="i-color fa-solid fa-comments"></i>
+                    <a href="#">12 Comments</a>
+                  </span>
+
+                  <!-- READ MORE BUTTON -->
+                  <span class="read-btn">
+                      <a href="">Read More</a>
+                  </span>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <!-- FINE CONTAINER ARTICLE -->
 
   </div>
 </template>
