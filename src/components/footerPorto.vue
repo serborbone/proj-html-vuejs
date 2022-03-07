bbcde5571b39009af281d60de9f38cc0b00d1af6
Sergio Borbone <sergioborbone@gmail.com>
2022-03-07 17:23:32 +0100

    Creato footer bottom

--- src/components/footerPorto.vue
+++ src/components/footerPorto.vue
@@ -2,7 +2,32 @@
   <footer>
       <div id="footerCol">
         <div class="container-w75">
+          <!-- FOOTER TOP -->
           <footerCol></footerCol>
+
+        </div>
+
+        <!-- FOOTER BOTTOM -->
+        <div class="footer-bottom">
+
+          <div class="container-w75 display-rules">
+
+            <div id="logo-footer">
+              <img src="../assets/img/logo-footer.png" alt="footer-logo">
+            </div>
+
+            <div class="copyright">
+                <i class="fa-regular fa-copyright"></i>
+                Copyright 2020. All Rights Reserved.
+            </div>
+
+            <div class="menu-foot-bottom">
+                <span><i class="i-style fa-solid fa-angle-right"></i><a href="">FAQ's</a></span>
+                <span><i class="i-style fa-solid fa-angle-right"></i><a href="">Sitemap</a></span>
+                <span><i class="i-style fa-solid fa-angle-right"></i><a href="">Contact Us</a></span>
+            </div>
+          </div>
+
         </div>
       </div>
   </footer>
@@ -23,9 +48,11 @@ export default {
 
 <style lang="scss" scoped>
 
+  @import '../assets/style/footerBottom.scss';
+
   #footerCol {
-    height: 500px;
-    background-color: #1c2023;
+    height: 400px;
+    background-color: #212529;
   }
 
 </style>
